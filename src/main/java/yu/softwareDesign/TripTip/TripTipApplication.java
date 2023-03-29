a2db69533eb6157a91134bf03d61f86f1bc77454
JaeSeo Yang <qud1251@gmail.com>
2023-03-29 14:41:08 +0900

    create project

---
+++ src/main/java/yu/softwareDesign/TripTip/TripTipApplication.java
@@ -0,0 +1,13 @@
+package yu.softwareDesign.TripTip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripTipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripTipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yu/softwareDesign/TripTip/TripTipApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.softwareDesign.TripTip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
